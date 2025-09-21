156435ada2e1de894c5cead49fc087b5de788001
Suhas Manjunatha <suhas.manjunatha.h@gmail.com>
2025-09-21 17:07:45 +0530

    Commit from Suhas after adding emergency button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,13 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.cardview:cardview:1.0.0")
+
+    // For speech recognition and TTS
+    implementation ("androidx.appcompat:appcompat:1.7.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/kenoha1/MainActivity.kt
+++ app/src/main/java/com/example/kenoha1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kenoha1
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.telecom.Call
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
@@ -14,17 +16,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kenoha1.ui.theme.Kenoha1Theme
 import android.widget.Button
+import android.speech.SpeechRecognizer
+import android.widget.TextView
 import android.widget.Toast
+import org.json.JSONObject
+import java.io.IOException
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val Button = findViewById<Button>(R.id.button1)
+        val quickAssistanceBtn = findViewById<Button>(R.id.button1)
+        val emergencyBtn = findViewById<Button>(R.id.button2)
 
-        Button.setOnClickListener {
-            Toast.makeText(this, "Hello User", Toast.LENGTH_SHORT).show()
+        // Navigate to AssistanceActivity
+        quickAssistanceBtn.setOnClickListener {
+            val intent = Intent(this, AssistanceActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Show a toast when emergency button is clicked
+        emergencyBtn.setOnClickListener {
+            Toast.makeText(this, "Emergency button clicked 🚨", Toast.LENGTH_SHORT).show()
         }
     }
 }
